a7e327213c1ff30db4ecfc7c77f8bebfbb862ecf
Lingyun Wu <wulingyun16@gmail.com>
2017-12-28 16:21:11 +0000

    12/28/2017

---
+++ MoreExpress/EJSDemo/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var app = express();
+
+
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+app.get("/fellinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
+})
+
+app.get("/posts", function(req, res) {
+   var posts = [
+       {title: "Post 1", author: "Susy"},
+       {title: "Post 2", author: "Tom"},
+       {title: "Post 3", author: "Sam"}
+       ]; 
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Sever has started!"); 
+});
