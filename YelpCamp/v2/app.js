21f786124f4f8f7ee7c9f921615ac334449b46d5
Lingyun Wu <wulingyun16@gmail.com>
2017-12-30 06:47:48 +0000

    YelpCamp DB Add

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,25 +1,37 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
-var campgrounds = [
-    {name: "Great Smoky Mountain", image:"https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7512/15558076393_8c1d4b9cf8.jpg"},
-    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://farm4.staticflickr.com/3922/15083214895_b0c5cf5e4e.jpg"},
-    {name: "Great Smoky Mountain", image:"https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7512/15558076393_8c1d4b9cf8.jpg"},
-    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://farm4.staticflickr.com/3922/15083214895_b0c5cf5e4e.jpg"},
-    {name: "Great Smoky Mountain", image:"https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg"},
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7512/15558076393_8c1d4b9cf8.jpg"},
-    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3832/9603531635_e348167e39.jpg"},
-    {name: "Mountain Goat's Rest", image:"https://farm4.staticflickr.com/3922/15083214895_b0c5cf5e4e.jpg"}
-];
+//Schema Setup
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Great Smoky Mountain", 
+//         image:"https://farm4.staticflickr.com/3706/10718494266_56543a9eb7.jpg",
+//         description: "LALALA"
+//     }, 
+//     function(err, campground) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(campground);
+//     }
+// });
+
+
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -27,7 +39,14 @@ app.get("/", function(req, res) {
 
 
 app.get ("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.render("index", {campgrounds: allCampgrounds});
+       }
+    });
 });
 
 
@@ -35,16 +54,35 @@ app.post("/campgrounds", function(req, res) {
    // Get data from form and add to the array 
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   
-   res.redirect("/campgrounds");
+   var descr = req.body.description;
+   var newCampground = {name: name, image: image, description: descr};
+   // Create a new campground and save it to DB
+   Campground.create(newCampground, function(err, newlyCreated) {
+      if (err) {
+          console.log(err);
+      } else {
+          res.redirect("/campgrounds");
+      }
+   });
 });
 
 
 app.get("/campgrounds/new", function(req, res) {
    res.render("new"); 
 });
+
+
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.render("show", {campground: foundCampground});
+       }
+    });
+});
+
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
--- YelpCamp/v2/readme.md
+++ YelpCamp/v2/readme.md
@@ -38,3 +38,11 @@ Each campground has:
 * Install and configure mongoose
 * Setup campground model
 * Use campground model inside of our routes!
+
+
+#Show Page
+
+* Review the RESTful routes we've seen so far
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show template/route
