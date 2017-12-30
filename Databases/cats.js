8b68d18adfd51fe4595fc9da8ebfd89566b61e4a
Lingyun Wu <wulingyun16@gmail.com>
2017-12-30 05:41:56 +0000

    database 2

--- Databases/cats.js
+++ Databases/cats.js
@@ -11,5 +11,41 @@ var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to the database
 
+var george  = new Cat({
+    name: "George",
+    age: 11,
+    temperament: "Grouchy"
+});
+
+george.save(function(err, cat) {
+    if (err) {
+        console.log("SOMETHING WENT WRONG!");
+    } else {
+        console.log(cat);
+    }
+});
+
+
+Cat.create({
+   name: "Snow White",
+   age: 15,
+   temperament: "Bland"
+}, function(err, cat) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(cat);
+    }
+});
+
 
 // retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats) {
+   if (err) {
+       console.log("OH NO, ERROR!");
+       console.log(err);
+   } else {
+       console.log(cats);
+   }
+});
---
+++ YelpCamp/v2/readme.md
@@ -0,0 +1,40 @@
+#YelpCamp
+
+* Add Landing Page
+* Add Campgrounds Page that lists all campgrounds
+
+
+Each campground has:
+    * Name
+    * Image
+    
+#Layout and Basic Styling
+
+* Create our header and footer partials
+* Add in Bootstrap
+
+
+
+#Creating New Campgrounds
+* Setup new campground POST route
+* Add in body parser
+* Setup route to show form
+* Add basic unstyled form
+
+
+#Styling the campgrounds page
+
+* Add a better header/title
+* Make campgrounds display in a grid
+
+#Styling the Navbar and Form
+
+* Add a navbar to all templates
+* Style the new template form
+
+
+#Add Mongoose
+
+* Install and configure mongoose
+* Setup campground model
+* Use campground model inside of our routes!
