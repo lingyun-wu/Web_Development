0730b64fd75314cb69fd546dd70019e14e7cad3d
Lingyun Wu <wulingyun16@gmail.com>
2018-01-05 03:44:28 +0000

    01/04/2018

--- YelpCamp/v8/routes/campgrounds.js
+++ YelpCamp/v8/routes/campgrounds.js
@@ -2,50 +2,53 @@ var express = require("express");
 var router  = express.Router();
 var Campground = require("../models/campground");
 
-router.get ("/", function(req, res) {
+//INDEX - show all campgrounds
+router.get("/", function(req, res){
     // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds) {
-       if (err) {
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
            console.log(err);
        } else {
-           res.render("campgrounds/index", {campgrounds: allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds});
        }
     });
 });
 
-
-router.post("/", function(req, res) {
-   // Get data from form and add to the array 
-   var name = req.body.name;
-   var image = req.body.image;
-   var descr = req.body.description;
-   var newCampground = {name: name, image: image, description: descr};
-   // Create a new campground and save it to DB
-   Campground.create(newCampground, function(err, newlyCreated) {
-      if (err) {
-          console.log(err);
-      } else {
-          res.redirect("/campgrounds");
-      }
-   });
+//CREATE - add new campground to DB
+router.post("/", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
-
-router.get("/new", function(req, res) {
+//NEW - show form to create new campground
+router.get("/new", function(req, res){
    res.render("campgrounds/new"); 
 });
 
-
-
-router.get("/:id", function(req, res) {
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-       if (err) {
-           console.log(err);
-       } else {
-           res.render("campgrounds/show", {campground: foundCampground});
-       }
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground)
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
     });
 });
-
 
 module.exports = router;
