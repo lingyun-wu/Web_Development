6cea517277ea2efc25e66b5efa3e6980346d541d
Lingyun Wu <wulingyun16@gmail.com>
2018-01-10 02:49:50 +0000

    01/09/2018

--- YelpCamp/v11/routes/campgrounds.js
+++ YelpCamp/v11/routes/campgrounds.js
@@ -20,13 +20,14 @@ router.get("/", function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: desc, author:author}
+    var newCampground = {name: name, price: price, image: image, description: desc, author:author}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -84,6 +85,7 @@ router.delete("/:id",middleware.checkCampgroundOwnership, function(req, res){
       if(err){
           res.redirect("/campgrounds");
       } else {
+          req.flash("success", "Campground deleted");
           res.redirect("/campgrounds");
       }
    });
--- YelpCamp/v11/routes/index.js
+++ YelpCamp/v11/routes/index.js
@@ -13,24 +13,29 @@ router.get("/", function(req, res) {
 router.get("/register", function(req, res){
    res.render("register"); 
 });
+
+
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            console.log(err);
+            req.flash("error", err.message);
             return res.render("register");
         }
         passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Welcome to YelpCamp" + user.username);
            res.redirect("/campgrounds"); 
         });
     });
 });
 
+
 // show login form
 router.get("/login", function(req, res){
    res.render("login", {message: req.flash("error")}); 
 });
+
 // handling login logic
 router.post("/login", passport.authenticate("local", 
     {
